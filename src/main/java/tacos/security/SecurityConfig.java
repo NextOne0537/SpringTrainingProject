0bd112c7d3882e42f7ca4ee56a358167631b7395
Marsik <timofeyzalesov@gmail.com>
2021-07-29 14:10:13 +0300

    fixed application not loading static content before user has been authorized

--- src/main/java/tacos/security/SecurityConfig.java
+++ src/main/java/tacos/security/SecurityConfig.java
@@ -1,9 +1,18 @@
 package tacos.security;
 
+
+
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 /**
  * @author Timofey
@@ -14,15 +23,52 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
                 .withUser("timofey")
-                .password("marsik")
+                .password(passwordEncoder().encode("marsik"))
                 .authorities("ROLE_USER")
                 .and()
+                .passwordEncoder(passwordEncoder())
                 .withUser("dima")
-                .password("marsik")
+                .password(passwordEncoder().encode("marsik"))
                 .authorities("ROLE_USER");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/login", "/", "/static/images/**", "/static/**", "/resources/**").permitAll()
+                .antMatchers("/design", "/orders/**", "/orders/current").authenticated()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .defaultSuccessUrl("/design")
+                .and()
+                .logout()
+                .logoutSuccessUrl("/");
+    }
+
+    /**This is required for application to load static content
+     * such as images to be loaded even before user has been authorised
+     * when we use spring security*/
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web
+                .ignoring()
+//                .antMatchers("/resources/**")
+                .antMatchers("/images/**");
+//                .antMatchers("/static/**")
+
     }
 }
--- src/main/java/tacos/web/WebConfig.java
+++ src/main/java/tacos/web/WebConfig.java
@@ -2,6 +2,7 @@ package tacos.web;
 
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -11,4 +12,13 @@ public class WebConfig implements WebMvcConfigurer {
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("home");
     }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**")
+                .addResourceLocations("classpath:/static/", "/","classpath:/static/**" );
+        WebMvcConfigurer.super.addResourceHandlers(registry);
+    }
+
+
 }
